ba8e6464ecbda48c586e76b6012a355372b76749
mugishakundasarah <mugishakundasarah@gmail.com>
2022-01-14 07:37:24 +0200

    tested the binary search function

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -4,24 +4,32 @@ using namespace std;
 int binary_search(int *arr, int arrSize, int searchItem){
     int low = 0;
     int high = arrSize - 1;
-    int mid = (low + high) / 2;
-    int guess = arr[mid];
+    int mid;
+    int guess;
     int result = -1;
 
-    while (low <= high)
+    while (low < high)
     {
+        if((low + high) % 2 != 0  && high!=1){
+            mid = (low + high + 1) / 2; 
+        }
+        else{
+            mid = (low + high) / 2;
+        }
+
+        guess = arr[mid];
+        
         if (guess == searchItem)
         {
             result = mid;
+            break;
         }
         else if(guess > searchItem)
         {
-            high = mid;
-            mid = (low + high) / 2; 
+            high = mid;    
         }
         else{
             low = mid;
-            mid = (low + high) / 2;
         }
     }
     
@@ -29,5 +37,16 @@ int binary_search(int *arr, int arrSize, int searchItem){
 }
 
 int main(){
+    int myList[5] = {1,3,5,7,9};
+    int res = binary_search(myList, 5, 9);
+
+    if(res == -1){
+        cout << "none" << endl;
+    }
+
+    else{
+        cout << res << endl;
+    }
+    return 0;
     
 }
