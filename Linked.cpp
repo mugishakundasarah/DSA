f321f14c22aeea28380ebaff414f7913030bec43
mugishakundasarah <mugishakundasarah@gmail.com>
2022-02-17 21:57:11 +0200

    added my own linked list

--- Linked.cpp
+++ Linked.cpp
@@ -22,10 +22,22 @@ class LinkedList{
             if (head == NULL)
             {
                 head = newNode;
+                return;
+            }
+            else{
+                Node* temp = head;
+                while (temp->next != NULL)
+                {
+                    temp = temp->next;
+                }
+                temp->next = newNode;
             }
         }
 };
 
 int main(){
     LinkedList list1;
+    list1.addNewNode(5);
+    list1.addNewNode(4);
+    list1.addNewNode(6);
 }
