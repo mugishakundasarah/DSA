0fb1d33e71e60993f0dde4f62c9574ed08f75229
mugishakundasarah <mugishakundasarah@gmail.com>
2022-06-03 20:47:07 +0200

    added gen tree

--- bsf_dsf.cpp
+++ bsf_dsf.cpp
@@ -1,7 +1,77 @@
 #include <iostream>
+#include <string>
 #include <list>
 #include <stack>
 #include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
+
+class Graph{
+	private:
+		int numNodes;
+		list<int> *adjList;
+		
+	public:
+		Graph(int numNodes){
+			this->numNodes = numNodes;
+			adjList = new list<int> [numNodes];
+		}
+		
+		void add_edge(int src, int dest){
+			adjList[src].push_back(dest);
+			adjList[dest].push_back(src);
+		}
+		
+		void display(){
+			for(int i=0; i<numNodes; i++){
+				cout << i << ": ";
+				for(auto node:adjList[i]){
+					cout << i << " --> " << node << "    ";
+				}
+				
+				cout << endl;
+			}
+		}
+	
+		void BFS(int startingVertex){
+			list<int> queue;
+			vector<bool> visited(numNodes, false);
+			
+			cout << "BFS result from " << startingVertex << ": " << endl;
+			queue.push_back(startingVertex);
+			visited[startingVertex] = true;
+			
+			while(!queue.empty()){
+				int current = queue.front();
+				cout << current << "\t";
+				for(auto i = adjList[current].begin(); i!=adjList[current].end(); ++i){
+					int adjVertex = *i;
+					if(!visited[adjVertex]){
+						queue.push_back(adjVertex);
+						visited[adjVertex] = true;
+					}
+				}
+				
+				visited[current] = true;
+				queue.pop_front();
+			}
+		}
+	
+};
+
+int main(){
+
+	Graph g(4);
+	g.add_edge(0, 1);
+	g.add_edge(1, 2);
+	g.add_edge(2, 3);
+	g.add_edge(3, 3);
+	g.add_edge(1, 3);
+	
+	g.display();
+	
+	cout << endl << endl;
+	g.BFS(2);
+
+	return 0;
+}
--- graph.cpp
+++ graph.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include <string>
+#include <list>
+#include <stack>
+#include <vector>
+#include <bits/stdc++.h>
+
 using namespace std;
 
 class AdjacencyMatrixGraph{
@@ -51,9 +57,95 @@ class AdjacencyMatrixGraph{
     }
 };
 
+class AdjacencyListGraph{
+    private: 
+        int nodes;
+        vector<int> *list;
+    public:
+     AdjacencyListGraph(int nodes){
+         this->nodes = nodes;
+         this->list = new vector<int>[nodes];
+    }
+
+    void display(){
+        for (int i = 0; i < nodes; i++)
+        {
+            for (int j = 0; j < list[i].size() ; j++)
+            {
+                cout << "->" << list[i][j];
+            }
+            cout << endl;
+        }
+               
+    }
+
+    void add_edge(int origin, int destination){
+        list[origin].push_back(destination);
+        list[destination].push_back(origin);
+    }
+    void BFS(int startingVertex){
+        vector<bool> visited(nodes, false);
+        std::list<int> queue;
+
+        cout << "BFS result from " << startingVertex << " : " << endl;
+        queue.push_back(startingVertex);
+        visited[startingVertex] = true;
+        while (!queue.empty())
+        {
+            int current = queue.front();
+            for(auto i=list[current].begin(); i!=list[current].end(); i++){
+                int adjVertex = *i;
+                if(!visited[adjVertex]){
+                    queue.push_back(adjVertex);
+                    visited[adjVertex] = true;
+                }
+            }
+            visited[current] = true;
+            queue.pop_front();
+        }
+        
+    }
+    void DFS(int startingVertex){
+        vector<bool> visited(nodes, false);
+        stack<int> stack;
+
+        stack.push(startingVertex);
+        cout << "DFS RESULT from " << startingVertex << ":" << endl;
+
+        while (!stack.empty())
+        {
+            int current = stack.top();
+            cout << current << " " << endl;
+            visited[current] = true;
+            stack.pop();
+            for(auto i=list[current].begin(); i!=list[current].end(); i++){
+                int adjVertex = *i;
+                if(!visited[adjVertex]){
+                    stack.push(adjVertex);
+                }
+            }
+        }
+               
+    }
+};
+
 int main(){
-    AdjacencyMatrixGraph adjacentMatrix(5);
-    adjacentMatrix.display();
-    adjacentMatrix.add_edge(2, 3);
-    adjacentMatrix.display();
+    // AdjacencyMatrixGraph adjacentMatrix(5);
+    // adjacentMatrix.display();
+    // adjacentMatrix.add_edge(2, 3);
+    // adjacentMatrix.display(); 
+
+    // AdjacencyListGraph list(5);
+    // list.add_edge(3, 2);
+    // list.display();
+
+    AdjacencyListGraph g(4);
+    g.add_edge(0, 1);
+    g.add_edge(0, 2);
+    g.add_edge(2, 3);
+    g.add_edge(2, 3);
+    g.add_edge(3, 3);
+    g.display();
+    g.BFS(1);
+    g.DFS(1);
 }
